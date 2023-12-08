bc82b00d8ead86579dbbc5c97b0a1f1747dc1979
Naochika Yonezumi <125625688+naochika19900512@users.noreply.github.com>
2023-12-08 16:34:34 +0900

    init-git
    初期ビルト

---
+++ ADServerSyncCard.ps1
@@ -0,0 +1,25 @@
+﻿$enabledUsers = Get-ADUser -Filter "*" -Properties samaccountname, employeeNumber, pager
+
+# 出力ファイルのパス
+$outputFilePath = "C:\path\outputfile.txt"
+$serverCom = "C:\Program Files\PaperCut MF\server\bin\win\server-command.exe"
+
+# ファイルに書き込むための空の文字列を作成
+$output = ""
+
+foreach ($user in $enabledUsers) {
+    $samaccountname = $user.samaccountname
+    $employeeNumber = $user.employeeNumber
+    $pager = $user.pager
+
+    # 結果を文字列に追加
+    $output += "$samaccountname`t$employeeNumber$pager`r`n"
+}
+
+# 結果をファイルに書き込み
+$output | Out-File -FilePath $outputFilePath -Encoding UTF8
+
+# サーバコマンドの実行
+cmd.exe /c $serverCom batch-import-user-card-id-numbers $outputFilePath TRUE
+
+
